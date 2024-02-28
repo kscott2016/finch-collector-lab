e9b041eb1e8ffd5d89c581d4f80fb2cf10f42c53
kscott2016 <krystinascott2008@yahoo.com>
2024-02-27 21:56:03 -0600

    add game index template and css

--- main_app/views.py
+++ main_app/views.py
@@ -3,6 +3,18 @@ from django.shortcuts import render
 # Add the following import
 from django.http import HttpResponse
 
+class Game:  # Note that parens are optional if not inheriting from another class
+  def __init__(self, name, description, release_year):
+    self.name = name
+    self.description = description
+    self.release_year = release_year
+
+games = [
+
+  Game('Kingdom Hearts', 'disney fighters and exploring', 2002),
+  Game('Crash Bandicoot: Warped', 'mutated bandicoot defeats scientist', 1998)
+]
+
 # Define the home view
 def home(request):
   return HttpResponse('<h1>Hello Gamer</h1>')
@@ -12,3 +24,6 @@ def home(request):
 
 def about(request):
   return render(request, 'about.html')
+
+def games_index(request):
+  return render(request, 'games/index.html', { 'games': games })
