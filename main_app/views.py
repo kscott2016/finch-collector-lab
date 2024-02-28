2e4ffe053ea120cfc5be28d515d7e40fb91b2f8f
kscott2016 <krystinascott2008@yahoo.com>
2024-02-27 21:25:06 -0600

    add css and logo

--- main_app/views.py
+++ main_app/views.py
@@ -7,5 +7,8 @@ from django.http import HttpResponse
 def home(request):
   return HttpResponse('<h1>Hello Gamer</h1>')
 
+# def about(request):
+#   return HttpResponse('<h1>About Gamer</h1>')
+
 def about(request):
-  return HttpResponse('<h1>About Gamer</h1>')
+  return render(request, 'about.html')
