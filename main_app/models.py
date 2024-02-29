4416f02ce8843300fd8aedb5273f015099d59e99
kscott2016 <krystinascott2008@yahoo.com>
2024-02-28 23:15:22 -0600

    add type model

--- main_app/models.py
+++ main_app/models.py
@@ -4,6 +4,14 @@ from django.db import models
 from django.db import models
 from django.urls import reverse
 
+TYPES = (
+  ('A', 'Adventure'),
+  ('F', 'Fantasy'),
+  ('R', 'RPG'),
+  ('B', 'Fighting'),
+  ('S', 'Shooter'),
+)
+
 class Game(models.Model):
   name = models.CharField(max_length=100)
   description = models.TextField(max_length=250)
@@ -14,3 +22,18 @@ class Game(models.Model):
 
   def get_absolute_url(self):
     return reverse('cat-detail', kwargs={'cat_id': self.id})
+  
+class Type(models.Model):
+  type = models.CharField(
+    max_length=1,
+    # add the 'choices' field option
+    choices=TYPES,
+    # set the default value for meal to be 'B'
+    default=TYPES[0][0])
+  max_players=models.IntegerField()
+
+  game = models.ForeignKey(Game, on_delete=models.CASCADE)
+
+  def __str__(self):
+    
+    return f"{self.get_types_display()}. Allows {self.max_players} to play"
